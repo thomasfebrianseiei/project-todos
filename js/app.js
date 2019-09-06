5e0ba1a9198d5c74d0c2c26ec6de46d1bf90cb1b
thomasfebrianseiei <thomas-f-seiei@hotmail.com>
2019-09-06 15:10:27 +0700

    add digital clock

--- js/app.js
+++ js/app.js
@@ -40,7 +40,29 @@ clear.addEventListener("click", function(){
     localStorage.clear();
     location.reload();
 });
-
+const getTime = () => {
+    const date = new Date();
+    let hours = date.getHours();
+    const amPm = hours >= 12 ? 'PM' : 'AM'; 
+    if (hours > 12) {
+      hours -= 12;
+    }
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const timeComponents = [hours, minutes, seconds, amPm]
+      .map(cmp => `<span>${String(cmp).padStart(2, '0')}</span>`);
+    return `${timeComponents.join(':')}`;
+  }
+  
+// show time
+  const init = () => {
+    setInterval(() => {
+      document.getElementById('time')
+        .innerHTML = getTime();
+    },1000);
+  };
+  
+  init();
 // Show todays date
 const options = {weekday : "long", month:"short", day:"numeric"};
 const today = new Date();
